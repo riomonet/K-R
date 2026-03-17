03a4cabc53ad1f324fa994514970aff87527e0cf
Ari Zablozki <ari@marina59.com>
2026-03-17 10:21:06 -0400

    adjust formatting

--- fold.c
+++ fold.c
@@ -11,42 +11,42 @@ char *fold(char *str) {
     int mod = 1;
     int lst_space = -1;
     while (str[str_p]) {
-	if (str[str_p] == ' ') {
-	    lst_space = out_p; 
-	}
-	if ((mod % MAX_COL) == 0) {
-	    if (lst_space == -1) {
-		output[out_p++] = '\n';
-		mod = 1;
-		continue;
-	    } else if (str[str_p] == ' ') {
-		output[out_p++] = '\n';
-		str_p++;
-		lst_space = -1;
-		mod = 1;
-		continue;
-	    } else {
-		output[lst_space] = '\n';
-		mod =  out_p - lst_space;
-		lst_space = -1;
-	    }
-	}
-	output[out_p] = str[str_p];
-	out_p++;
-	str_p++;
-	mod++;
+        if (str[str_p] == ' ') {
+            lst_space = out_p; 
+        }
+        if ((mod % MAX_COL) == 0) {
+            if (lst_space == -1) {
+                output[out_p++] = '\n';
+                mod = 1;
+                continue;
+            } else if (str[str_p] == ' ') {
+                output[out_p++] = '\n';
+                str_p++;
+                lst_space = -1;
+                mod = 1;
+                continue;
+            } else {
+                output[lst_space] = '\n';
+                mod =  out_p - lst_space;
+                lst_space = -1;
+            }
+        }
+        output[out_p] = str[str_p];
+        out_p++;
+        str_p++;
+        mod++;
     } 
     return &output[1];
 }
 
 void print(char *str) {
     while(*str) {
-	if(*str == '\n') {
-	    printf("|");
-	} else {
-	    printf("%c",*str);
-	}
-	str++;
+        if(*str == '\n') {
+            printf("|");
+        } else {
+            printf("%c",*str);
+        }
+        str++;
     }
     printf("\n");
 }
